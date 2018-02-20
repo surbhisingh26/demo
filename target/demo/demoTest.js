7d183d582d3377b21ef8aea9594055d9ccf1d26a
surbhi <surbhi.singh.ss05@gmail.com>
2018-02-20 13:28:58 +0530

    chng

--- target/demo/demoTest.js
+++
@@ -1,9 +0,0 @@
-casper.test.begin('The heading exists', 2, function suite(test) {
-    casper.start('http://localhost:8080/demo/demo.jsp', function() {
-    	test.assertExists('h1.hello');
-    	test.assertSelectorHasText('h1.hello', 'Hello surbhi');
-    	
-    }).run(function() {
-        test.done();
-    });
-});
