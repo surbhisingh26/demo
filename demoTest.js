8a99e87f943638157fa255f14e405a5279136f6a
surbhi <surbhi.singh.ss05@gmail.com>
2018-02-20 15:39:54 +0530

    chng

--- demoTest.js
+++ demoTest.js
@@ -1,7 +1,7 @@
 casper.test.begin('The heading exists', 2, function suite(test) {
     casper.start('http://localhost:8080/demo/demo.jsp', function() {
     	test.assertExists('h1.hello');
-    	test.assertSelectorHasText('h1.hello', 'Hello surbhi');
+    	test.assertSelectorHasText('h1.hello', 'Hello surbi');
     	
     }).run(function() {
         test.done();
